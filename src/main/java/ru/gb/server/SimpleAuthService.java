2d765c77e236917d6744d27ab5fc71b8ac03c9bc
IlyaM <ilya.mostolygin@gmail.com>
2021-11-28 18:04:42 +0300

    авторизация через бд

--- src/main/java/ru/gb/server/SimpleAuthService.java
+++ src/main/java/ru/gb/server/SimpleAuthService.java
@@ -1,36 +1,44 @@
 package ru.gb.server;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.sql.*;
 
 public class SimpleAuthService implements AuthService {
 
-    private final List<UserData> users;
+    private Connection connection;
 
     public SimpleAuthService() {
-        users = new ArrayList<>();
-        users.add(new UserData("login1", "pass1", "nick1"));
-        users.add(new UserData("login2", "pass2", "nick2"));
-        users.add(new UserData("login3", "pass3", "nick3"));
     }
 
     @Override
     public String getNickByLoginAndPassword(String login, String password) {
-        for (UserData user : users) {
-            if (user.login.equals(login) && user.password.equals(password)) return user.nick;
+        try {
+            connect();
+            try (PreparedStatement ps = connection.prepareStatement("" +
+                    "select nick from users where login = ? and password = ?")) {
+                ps.setString(1, login);
+                ps.setString(2, password);
+                ResultSet rs = ps.executeQuery();
+                if (rs.next()) return rs.getString(1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            disconnect();
         }
         return null;
     }
 
-    private static class UserData {
-        private final String login;
-        private final String password;
-        private final String nick;
+    private void connect() throws SQLException {
+        connection = DriverManager.getConnection("jdbc:sqlite:auth.db");
+    }
 
-        public UserData(String login, String password, String nick) {
-            this.login = login;
-            this.password = password;
-            this.nick = nick;
+    public void disconnect() {
+        if(connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
